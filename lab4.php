e65afee6c0f2d20c4c8f998af5d757e3ee017aee
Manonchl <47968499+Manonchl@users.noreply.github.com>
2019-03-25 08:57:31 +0100

    Add files via upload

---
+++ lab4.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+    <link rel="Stlye" href="lab4style.css">
+</head>
+<body>
+    <h1>Friend book</h1>
+    <form action="index.php" method="get">
+Name: <input type="text" name="name">
+<input type="add  new friends">
+</form>
